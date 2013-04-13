d32a48b64d19799bd8bb38e6744fc9b61878e258
Vyacheslav Yasevich <syasevich@mail.ru>
2013-04-13 22:33:12 +0700

    Missing permission has been added. RingerModeBroadcastReceiver is implemented.

---
+++ src/com/synergy/android/timetable/receivers/RingerModeBroadcastReceiver.java
@@ -0,0 +1,20 @@
+package com.synergy.android.timetable.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+
+import com.synergy.android.timetable.TimetableApplication;
+
+public class RingerModeBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getExtras().getInt(AudioManager.EXTRA_RINGER_MODE) !=
+                AudioManager.RINGER_MODE_SILENT) {
+            ScheduleBroadcastReceiver.cancelRingerModeService(context);
+            ScheduleBroadcastReceiver.scheduleRingerModeService(context,
+                    TimetableApplication.ACTION_RINGER_MODE_SILENT);
+        }
+    }
+}
--- src/com/synergy/android/timetable/services/RingerModeService.java
+++ src/com/synergy/android/timetable/services/RingerModeService.java
@@ -4,8 +4,10 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.AudioManager;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
@@ -13,11 +15,14 @@ import android.support.v4.app.NotificationCompat;
 import com.synergy.android.timetable.ApplicationSettings;
 import com.synergy.android.timetable.R;
 import com.synergy.android.timetable.TimetableApplication;
+import com.synergy.android.timetable.receivers.RingerModeBroadcastReceiver;
 import com.synergy.android.timetable.receivers.ScheduleBroadcastReceiver;
 
 public class RingerModeService extends Service {
     private static final String ACTION_RESET_MODE =
             "com.synergy.android.timetable.intent.action.RESET_MODE";
+    
+    private static BroadcastReceiver receiver;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -26,20 +31,18 @@ public class RingerModeService extends Service {
     
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        ApplicationSettings settings = ApplicationSettings.getInstance(this);
-        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
-        NotificationManager notificationManager = (NotificationManager)
-                getSystemService(NOTIFICATION_SERVICE);
-        
         String action = intent.getAction();
         if (action.equals(TimetableApplication.ACTION_RINGER_MODE_SILENT)) {
-            showNotification(notificationManager);
+            showNotification();
+            ApplicationSettings settings = ApplicationSettings.getInstance(this);
+            AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
             settings.setPreviousRingerMode(audioManager.getRingerMode());
             audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+            registerReceiver(this);
             ScheduleBroadcastReceiver.scheduleRingerModeService(this,
                     TimetableApplication.ACTION_RESET_RINGER_MODE);
         } else if (action.equals(TimetableApplication.ACTION_RESET_RINGER_MODE)) {
-            resetRingerMode(notificationManager, audioManager, settings);
+            resetRingerMode(this);
             ScheduleBroadcastReceiver.scheduleRingerModeService(this,
                     TimetableApplication.ACTION_RINGER_MODE_SILENT);
             stopSelf();
@@ -54,16 +57,13 @@ public class RingerModeService extends Service {
     }
     
     public static void resetRingerMode(Context context) {
-        ApplicationSettings settings = ApplicationSettings.getInstance(context);
-        AudioManager audioManager = (AudioManager) context.getSystemService(AUDIO_SERVICE);
         NotificationManager notificationManager = (NotificationManager)
                 context.getSystemService(NOTIFICATION_SERVICE);
-        resetRingerMode(notificationManager, audioManager, settings);
-    }
-    
-    private static void resetRingerMode(NotificationManager notificationManager,
-            AudioManager audioManager, ApplicationSettings settings) {
         cancelNotification(notificationManager);
+        unregisterReceiver(context);
+        
+        ApplicationSettings settings = ApplicationSettings.getInstance(context);
+        AudioManager audioManager = (AudioManager) context.getSystemService(AUDIO_SERVICE);
         audioManager.setRingerMode(settings.getPreviousRingerMode());
         settings.setPreviousRingerMode(-1);
     }
@@ -72,7 +72,10 @@ public class RingerModeService extends Service {
         notificationManager.cancel(TimetableApplication.RINGER_MODE_NOTIFICATION_ID);
     }
     
-    private void showNotification(NotificationManager notificationManager) {
+    private void showNotification() {
+        NotificationManager notificationManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+        
         Intent intent = new Intent(this, RingerModeService.class);
         intent.setAction(ACTION_RESET_MODE);
         PendingIntent pendingIntent = PendingIntent.getService(this, 0, intent, 0);
@@ -88,5 +91,20 @@ public class RingerModeService extends Service {
         
         notificationManager.notify(TimetableApplication.RINGER_MODE_NOTIFICATION_ID,
                 notification);
+    }
+    
+    private static synchronized void registerReceiver(Context context) {
+        if (receiver == null) {
+            receiver = new RingerModeBroadcastReceiver();
+            IntentFilter filter = new IntentFilter(AudioManager.RINGER_MODE_CHANGED_ACTION);
+            context.registerReceiver(receiver, filter);
+        }
+    }
+    
+    private static synchronized void unregisterReceiver(Context context) {
+        if (receiver != null) {
+            context.unregisterReceiver(receiver);
+            receiver = null;
+        }
     }
 }
